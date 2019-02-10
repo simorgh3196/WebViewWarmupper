b74b09f22b5dc896beac44e6c9a63733406f8e6a
simorgh3196 <simorgh3196@gmail.com>
2019-02-10 15:45:31 +0900

    setup tests

--- UnitTests/WebViewWarmuperTests.swift
+++ UnitTests/WebViewWarmuperTests.swift
@@ -1,5 +1,5 @@
 //
-//  WebViewWarmuperTests.swift
+//  ViewWarmuperTests.swift
 //  WebViewWarmuperTests
 //
 //  Created by Tomoya Hayakawa on 2019/02/10.
@@ -9,26 +9,13 @@
 import XCTest
 @testable import WebViewWarmuper
 
-class WebViewWarmuperTests: XCTestCase {
+class ViewWarmuperTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
     func testPerformanceExample() {
-        // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
         }
     }
-
 }
